4b8c6a288f02c974f5f0f815cc8df2506d4d9581
claredowson <clare.dowson@barnardos.org.uk>
2020-01-28 15:38:49 +0000

    my first commit

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+
+module.exports = () => ({
+plugins: [
+  require("postcss-import"),
+  require("postcss-preset-env")({
+    autoprefixer: {},
+    features: {
+      "focus-within": true,
+      "nesting-rules": true,
+      "color-mod-function": {
+        unresolved: "warn"
+      },
+      "custom-properties": {
+        preserve: false,
+        warnings: true
+      }
+    }
+  }),
+  require("postcss-browser-reporter"),
+  require("postcss-reporter")
+]
+});
